9cd7d514304ed615a0855e4a71821cd9c90ef8c3
张呈成 <360467435@qq.com>
2021-02-02 17:05:35 +0800

    集群容错--cluster --并行调用多个服务提供者ForkingClusterInvoker -触发调用doInvoke()

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/ForkingClusterInvoker.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/ForkingClusterInvoker.java
@@ -59,19 +59,29 @@ public class ForkingClusterInvoker<T> extends AbstractClusterInvoker<T> {
         super(directory);
     }
 
+    /**
+     * ForkingClusterInvoker 会在运行时通过线程池创建多个线程，并发调用多个服务提供者。只要有一个服务提供者成功返回了结果，
+     * doInvoke 方法就会立即结束运行。ForkingClusterInvoker 的应用场景是在一些对实时性要求比较高读操作
+     * （注意是读操作，并行写操作可能不安全）下使用，但这将会耗费更多的资源
+     */
     @Override
     @SuppressWarnings({"unchecked", "rawtypes"})
     public Result doInvoke(final Invocation invocation, List<Invoker<T>> invokers, LoadBalance loadbalance) throws RpcException {
         try {
             checkInvokers(invokers, invocation);
             final List<Invoker<T>> selected;
+            // 获取 forks 配置
             final int forks = getUrl().getParameter(FORKS_KEY, DEFAULT_FORKS);
+            // 获取超时配置
             final int timeout = getUrl().getParameter(TIMEOUT_KEY, DEFAULT_TIMEOUT);
+            // 如果 forks 配置不合理，则直接将 invokers 赋值给 selected
             if (forks <= 0 || forks >= invokers.size()) {
                 selected = invokers;
             } else {
                 selected = new ArrayList<>(forks);
+                // 循环选出 forks 个 Invoker，并添加到 selected 中
                 while (selected.size() < forks) {
+                    // 选择 Invoker
                     Invoker<T> invoker = select(loadbalance, invocation, invokers, selected);
                     if (!selected.contains(invoker)) {
                         //Avoid add the same invoker several times.
@@ -82,25 +92,38 @@ public class ForkingClusterInvoker<T> extends AbstractClusterInvoker<T> {
             RpcContext.getContext().setInvokers((List) selected);
             final AtomicInteger count = new AtomicInteger();
             final BlockingQueue<Object> ref = new LinkedBlockingQueue<>();
+            // 遍历 selected 列表
             for (final Invoker<T> invoker : selected) {
+                // 为每个 Invoker 创建一个执行线程
                 executor.execute(() -> {
                     try {
+                        // 进行远程调用
                         Result result = invoker.invoke(invocation);
+                        // 将结果存到阻塞队列中
                         ref.offer(result);
                     } catch (Throwable e) {
                         int value = count.incrementAndGet();
+                        // 仅在 value 大于等于 selected.size() 时，才将异常对象放入阻塞队列中，为什么这样做？
+                        // 在并行调用多个服务提供者的情况下，只要有一个服务提供者能够成功返回结果，而其他全部失败。
+                        // 此时 ForkingClusterInvoker 仍应该返回成功的结果，而非抛出异常。
+                        // 在value >= selected.size()时将异常对象放入阻塞队列中，可以保证异常对象不会出现在正常结果的前面，
+                        // 这样可从阻塞队列中优先取出正常的结果。
                         if (value >= selected.size()) {
+                            // 将异常对象存入到阻塞队列中
                             ref.offer(e);
                         }
                     }
                 });
             }
             try {
+                // 从阻塞队列中取出远程调用结果
                 Object ret = ref.poll(timeout, TimeUnit.MILLISECONDS);
+                // 如果结果类型为 Throwable，则抛出异常
                 if (ret instanceof Throwable) {
                     Throwable e = (Throwable) ret;
                     throw new RpcException(e instanceof RpcException ? ((RpcException) e).getCode() : 0, "Failed to forking invoke provider " + selected + ", but no luck to perform the invocation. Last error is: " + e.getMessage(), e.getCause() != null ? e.getCause() : e);
                 }
+                // 返回结果
                 return (Result) ret;
             } catch (InterruptedException e) {
                 throw new RpcException("Failed to forking invoke provider " + selected + ", but no luck to perform the invocation. Last error is: " + e.getMessage(), e);

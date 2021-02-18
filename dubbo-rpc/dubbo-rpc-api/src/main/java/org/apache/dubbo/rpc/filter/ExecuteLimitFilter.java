8a0ba35afb6f1acdf9adebbbccf4847c2016f299
张呈成 <360467435@qq.com>
2021-02-18 22:44:27 +0800

    dubbo 限流 之executes

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/ExecuteLimitFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/ExecuteLimitFilter.java
@@ -39,6 +39,20 @@ public class ExecuteLimitFilter implements Filter, Filter.Listener {
 
     private static final String EXECUTE_LIMIT_FILTER_START_TIME = "execute_limit_filter_start_time";
 
+    /**
+     * dubbo 限流 之executes
+     * 1、生产端线程池
+        多线程并发操作一定离不开线程池，Dubbo自身提供了支持了四种线程池类型支持。
+        生产者<dubbo:protocol>标签中可配置线程池关键参数，线程池类型、阻塞队列大小、核心线程数量等
+     iothreads：限制的是io线程池大小，该线程池线程用于处理Dubbo框架自身业务逻辑。默认值为CPU+1，不建议更改设置
+     threads：用于指定下面讲到的业务线程池线程数量，这个才是业务需要关心的线程数量。默认大小200
+     threadpool：指定使用线程池类型，Dubbo中自身实现提供了四种线程池。默认使用固定大小线程池FixedThreadPool
+      2、executes 一个只能在生产者即dubbo:service亦或是其子标签dubbo:method中配置的属性，消费者中配置不会生效。
+      这个参数主要目的是在生产者端限制应用线程使用数量
+     3、如：
+     服务器端并发执行（或占用线程池线程数）不能超过 10 个
+     <dubbo:service interface="com.foo.BarService" executes="10" />
+     */
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         URL url = invoker.getUrl();
